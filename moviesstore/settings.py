9ede9978b449e11ff096fa51ecd3b354ff4e74db
padraiglittlefield <padraiglittlefield@gmail.com>
2025-09-16 15:25:44 +0000

    Made fixes for pythonanywhere

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!nubvu@0e$p^r$p_vrv6!)(8c(lye%3m3fen12@5gp+(3!l4qm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
