b720e7ec38bde7ed8ea55369df20ed730afb8648
padraiglittlefield <padraiglittlefield@gmail.com>
2025-10-03 17:24:20 -0400

    practically

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'movies',
     'accounts',
     'cart',
-    'petitions',
+    'petitions'
 ]
 
 MIDDLEWARE = [
--- petitions/urls.py
+++ petitions/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='petitions.index'),
     path('create/', views.create, name='petitions.create'),
-    path('<int:id>/', views.show, name='petitions.show'),
-    path('<int:id>/vote/', views.vote, name='petitions.vote')
+    path('<int:id>/vote/', views.vote, name='petitions.vote'),
 ]
